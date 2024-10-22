a86fa0419ae238baf34ce49a544fbdf832c84c0c
风兮木晓 <103013728+1nCharles@users.noreply.github.com>
2024-10-22 14:43:03 +0800

    Add files via upload

---
+++ Features/XP.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace LiarsBarEnhance.Features;
+    [HarmonyPatch]
+    public class XP
+    {
+        [HarmonyPatch(typeof(DatabaseManager), "Start")]
+        [HarmonyPostfix]
+        public static void BlorfGamePlay_Look(DatabaseManager __instance)
+        {
+            Traverse.Create(__instance).Field("xp").SetValue(999999);
+            int wins = Traverse.Create(__instance).Field("Wins").GetValue<int>();
+            Debug.Log($"{wins}");
+        }
+    }
